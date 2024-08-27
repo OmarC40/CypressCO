dbbf9d8d525daf17c0dfc36ccb051caa89b673f7
OmarCuarenta <50001593+OmarC40@users.noreply.github.com>
2024-08-26 20:52:25 -0600

    New

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+//Es lo pirmero que pondremos para inicar nuestra test suite
+describe('Test Suite',()=>{
+
+    describe('Seccion adentro de la suite',()=>{
+
+        beforeEach('login',()=>{
+
+        })
+
+        it('primera prueba',()=>{
+            //aqui se pone el codigo que remos
+        })
+        it('segunda prueba',()=>{
+            //aqui se pone el codigo que remos
+        })
+        it('tercera prueba',()=>{
+            //aqui se pone el codigo que remos
+        })
+        it('cuarta prueba',()=>{
+            //aqui se pone el codigo que remos
+        })
+    })
+
+    it('primera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('segunda prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('tercera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('cuarta prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+})
+
+describe('Test Suite',()=>{
+    it('primera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('segunda prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('tercera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('cuarta prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+})
+
+
+// final video Test Structure
+
+
+describe('Test Suite',()=>{
+    it('primera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('segunda prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('tercera prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+    it('cuarta prueba',()=>{
+        //aqui se pone el codigo que remos
+    })
+})
